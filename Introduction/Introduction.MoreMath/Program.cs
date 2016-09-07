2919e6ed0e21d3003979fb8fb7ba1a93e965299a
Martynas Kadiša <candylifter@gmail.com>
2016-09-08 00:01:23 +0300

    Add MoreMath project

---
+++ Introduction/Introduction.MoreMath/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction.MoreMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //          { ae^-x, x<=0
+            // f(x) =   { 5ax - 7, 0 < x < 1
+            //          { sqrt(x+1)
+
+            int a;
+            double x;
+            double functionResult;
+
+            Console.WriteLine("Iveskite a reiksme:");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Iveskite x reiksme:");
+            x = double.Parse(Console.ReadLine());
+
+            if ( x <= 0 )
+            {
+                functionResult = a * Math.Exp(-x);
+            }
+            else if ( x < 1 )
+            {
+                functionResult = 5 * a * x - 7;
+            }
+            else
+            {
+                functionResult = Math.Pow(x + 1, 0.5);
+            }
+
+            Console.WriteLine(" Reiksme a = {0}, x = {1}, f(x) = {2}", a, x, functionResult);
+            Console.ReadKey();
+    
+        }
+    }
+}
