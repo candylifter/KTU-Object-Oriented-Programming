9e7c7cff96343391f0570d747842d664e8d55598
Martynas Kadiša <candylifter@gmail.com>
2016-09-09 09:24:51 +0300

    Add methods step2  and 3

---
+++ Introduction/Introduction.Methods.Step2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction.Methods.Step2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double functionResult;
+            int a;
+            double x;
+
+            Console.WriteLine("Įveskite a reiškmę");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Įveskite x reiškmę");
+            x = double.Parse(Console.ReadLine());
+
+            functionResult = CalculateFunctionValue(a, x);
+            Console.WriteLine(" Reikšmė a = {0}, reiškmė x = {1}, fx = {2}", a, x, functionResult);
+            Console.ReadKey();
+        }
+
+        static double CalculateFunctionValue(int a, double x)
+        {
+            double value;
+
+            if (x <= 0)
+            {
+                value = a * Math.Exp(-x);
+            }
+            else if (x < 1)
+            {
+                value = 5 * a * x - 7;
+            }
+            else
+            {
+                value = Math.Pow(x + 1, 0.5);
+            }
+
+            return value;
+        }
+    }
+}
